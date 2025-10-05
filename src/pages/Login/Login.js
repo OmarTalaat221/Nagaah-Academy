58cd4385826fb702de9824eac303dfa7d8726d7f
Tal3at <omartalaat1276@gmail.com>
2025-10-05 16:07:28 +0300

    Modification protected

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -234,28 +234,32 @@ const Login = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5 }}
       >
-        <div
-          className={`${
-            isIOS
-              ? "bg-blue-100 border-blue-400 text-blue-800"
-              : "bg-yellow-100 border-yellow-400 text-yellow-800"
-          } px-4 py-2 rounded-lg text-sm text-center shadow-lg border`}
-        >
-          <svg
-            className="w-4 h-4 inline-block ml-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
+        {isIOS ? (
+          <></>
+        ) : (
+          <div
+            className={`${
+              isIOS
+                ? "bg-blue-100 border-blue-400 text-blue-800"
+                : "bg-yellow-100 border-yellow-400 text-yellow-800"
+            } px-4 py-2 rounded-lg text-sm text-center shadow-lg border`}
           >
-            <path
-              fillRule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-              clipRule="evenodd"
-            />
-          </svg>
-          {/* {isIOS
-            ? "الإشعارات غير متاحة على أجهزة iOS"
-            : "يجب السماح بالإشعارات لتسجيل الدخول"} */}
-        </div>
+            <svg
+              className="w-4 h-4 inline-block ml-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {isIOS
+              ? "الإشعارات غير متاحة على أجهزة iOS"
+              : "يجب السماح بالإشعارات لتسجيل الدخول"}
+          </div>
+        )}
       </motion.div>
 
       {/* Animated Background Elements */}
